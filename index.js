24c08cf9d8641a6b20bb4b15c8e0d6e3cb7af6b7
ErnestoXMC <ernestopmc11@gmail.com>
2025-08-05 11:54:17 -0500

    feat: Se agrego la configuracion de CORS

--- index.js
+++ index.js
@@ -3,15 +3,32 @@ import dotenv from "dotenv";
 import conectarDB from "./config/db.js";//Archivos creados por nosotros necesitan la extension .js
 import veterinarioRoutes from "./routes/veterinarioRoutes.js";
 import pacienteRoutes from "./routes/pacienteRoutes.js";
+import cors from "cors";
 
 const app = express();
 //Permite leer correctamente los datos JSON que nos mandan en request en el controlador
 app.use(express.json());
+
 //Obtendremos las variables de entorno
 dotenv.config();
 
 //Conexion a la BD
 conectarDB();
+
+//Configuracion de cors
+const dominiosPermitidos = ["http://localhost:5173"];
+
+const corsOptions = {
+    origin: function(origin, callback){
+        if(dominiosPermitidos.indexOf(origin) !== -1){
+            callback(null, true);
+        } else {
+            callback(new Error("No permitido por CORS Policity"));
+        }
+    }
+}
+
+app.use(cors(corsOptions))
 
 //Ruta
 app.use('/api/veterinarios', veterinarioRoutes);
